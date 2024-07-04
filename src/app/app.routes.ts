914c56c85bf53f248b44aaaa27448c1f9626120d
Molnár Ákos <mabphun@gmail.com>
2024-07-04 16:05:16 +0200

    Added content to the home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 export const routes: Routes = [
     { path: 'home', component: HomeComponent },
     { path: 'projects', component: ProjectsComponent },
+    { path: 'gallery', component: GalleryComponent },
     { path: '**', redirectTo: 'home', pathMatch: 'full'}
 ];
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,11 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatCardModule, MatButtonModule],
+  imports: [
+    CommonModule, 
+    MatCardModule, 
+    MatButtonModule,
+    MatExpansionModule,
+    MatIconModule,
+    RouterModule
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
