89fb6217c979b8d1e1d2f4f2be7d4fcdf14da9c8
Molnár Ákos <33196966+mabphun@users.noreply.github.com>
2024-09-28 03:31:37 +0200

    Fixed hashed routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,7 +8,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes, 
-      //withHashLocation()
+      withHashLocation()
     ), 
     provideAnimationsAsync()
   ]
